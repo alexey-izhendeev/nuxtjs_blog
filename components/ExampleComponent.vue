7621a6a2ddfd7218517049b08ba3e115a3062370
Alexey Izhendeev <takedo19@gmail.com>
2021-07-09 15:04:55 +0700

    made contact page

---
+++ components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1> This component is a child </h1>
+        <input type="text" placeholder="Input a text to pass to a page" v-model="text">
+        <button @click="sendTopage"> Send to page </button>
+        <h1> End of child </h1>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            text: "",
+        }
+    },
+    methods: {
+        sendTopage() {
+            this.$emit('sendTopage', this.text)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/example.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       <ExampleComponent @sendTopage="data_from_component = $event" />
+       <h1> This is parent page </h1>
+       <p> This is data from component <b> {{data_from_component}}</b></p>
+    </div>  
+</template>
+
+<script>
+import ExampleComponent from "@/components/ExampleComponent";
+
+export default {
+   components: {
+       ExampleComponent,
+   },
+   data() {
+       return {
+           data_from_component: ""
+       }
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
